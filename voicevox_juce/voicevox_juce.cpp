3c99ca42618de00d3cb38e6b07febdc0dc4cd20c
Tatsuya Shiozawa <tatsuya.shiozawa@cocotone.jp>
2024-07-10 01:21:22 +0900

    Add comment.

--- voicevox_juce/voicevox_juce.cpp
+++ voicevox_juce/voicevox_juce.cpp
@@ -1,5 +1,6 @@
 #include "voicevox_juce.h"
 
+//==============================================================================
 // Hosting object of voicevox_core library
 #include "voicevox_core_host/voicevox_core_host.cpp"
 #include "voicevox_client/voicevox_client.cpp"
